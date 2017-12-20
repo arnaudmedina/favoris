f5bf492333d3ab44ac6f7c5146ab907ba396adbb
Mathieu KIM ROBIN <MathRobin@users.noreply.github.com>
2017-12-20 09:10:31 +0100

    Update formulaireEtatCivil.js

--- src/main/webapp/resources/js/formulaireEtatCivil.js
+++ src/main/webapp/resources/js/formulaireEtatCivil.js
@@ -24,8 +24,7 @@ function changeListenerEtatCivil(event, firstCall) {
 	   var re=/^(0|\+33)[1-9]([-. ]?[0-9]{2}){4}$/;
 	}
 	if ("numMobile" == this.id ) {
-	var re=/^((\+|00)33\s?|0)[67](\s?\d{2}){4}$/u
-	;
+		var re=/^((\+|00)33\s?|0)[67](\s?\d{2}){4}$/u;
 	}
 	var OK = re.exec(this.value);
 	if (!OK) { 
